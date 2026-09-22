fb156d49d4a12cdf69cf899c7667cc797ed8762d
Andy R <andy@azureussolutions.com>
2026-09-22 16:14:58 +0100

    Avoid unused display state in headless U-Boot

--- board/atmel/sama5d27_nextgen/sama5d27_nextgen.c
+++ board/atmel/sama5d27_nextgen/sama5d27_nextgen.c
@@ -30,10 +30,9 @@ DECLARE_GLOBAL_DATA_PTR;
 #ifdef CONFIG_BOARD_LATE_INIT
 int board_late_init(void)
 {
+#if defined(CONFIG_VIDEO_ST7789_SPI)
 	struct udevice *bus, *child;
 	int ret;
-
-#if defined(CONFIG_VIDEO_ST7789_SPI)
 	ret = uclass_get_device_by_seq(UCLASS_SPI, 1, &bus);
 	if (ret) {
 		printf("SPI bus 1 not found: %d\n", ret);
