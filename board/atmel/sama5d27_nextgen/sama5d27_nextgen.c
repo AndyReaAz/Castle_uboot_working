f0c2e07b7e17972ebc00bd1d0f768dbee87489fe
Andy R <andy@azureussolutions.com>
2026-09-22 16:14:22 +0100

    Keep U-Boot display init disabled when video is off

--- board/atmel/sama5d27_nextgen/sama5d27_nextgen.c
+++ board/atmel/sama5d27_nextgen/sama5d27_nextgen.c
@@ -33,6 +33,7 @@ int board_late_init(void)
 	struct udevice *bus, *child;
 	int ret;
 
+#if defined(CONFIG_VIDEO_ST7789_SPI)
 	ret = uclass_get_device_by_seq(UCLASS_SPI, 1, &bus);
 	if (ret) {
 		printf("SPI bus 1 not found: %d\n", ret);
@@ -43,14 +44,11 @@ int board_late_init(void)
 		if (ofnode_device_is_compatible(dev_ofnode(child),
 						"sitronix,st7789v-rgb-init")) {
 			ret = device_probe(child);
-			// printf("ST7789 probe ret=%d\n", ret);
 			break;
 		}
 	}
-
-#ifdef CONFIG_VIDEO
-	//at91_video_show_board_info();
 #endif
+
 	at91_pda_detect();
 	return 0;
 }
