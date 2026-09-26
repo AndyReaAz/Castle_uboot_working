e6276e9f6b64daa16e15aed884047b8c51a1df57
Andy R <andy@azureussolutions.com>
2026-09-26 16:40:27 +0100

    Force NextGen backlight off in U-Boot board init

--- board/atmel/sama5d27_nextgen/sama5d27_nextgen.c
+++ board/atmel/sama5d27_nextgen/sama5d27_nextgen.c
@@ -74,6 +74,13 @@ void board_debug_uart_init(void)
 
 int board_init(void)
 {
+	/*
+	 * PC28 is the LCD backlight control.  Keep it hard-off throughout
+	 * U-Boot regardless of GPIO-hog probe timing; Linux remuxes the pin to
+	 * LCDPWM when its display/backlight driver takes ownership.
+	 */
+	atmel_pio4_set_pio_output(AT91_PIO_PORTC, 28, 0);
+
 	/* address of boot parameters */
 	gd->bd->bi_boot_params = gd->bd->bi_dram[0].start + 0x100;
 
